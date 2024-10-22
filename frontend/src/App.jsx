5c65832d4bde01bbcb5d4edeea15e065ba1fe491
sadneya145 <sadneyasam05@gmail.com>
2024-10-22 11:16:04 +0530

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import InterestCalculator from './IntrestCalculator';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Savings Interest Calculator</h1>
+      <InterestCalculator />
+    </div>
+  );
+};
+
+export default App;
